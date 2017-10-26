6c28cb4c9b49129a540ba4aca72ed43a3685507e
sumantapatel <sumant.patel@gmail.com>
2017-10-26 13:58:44 +0530

    Third Commit

--- src/main/java/com/spatel/swagger/swaggerexample/config/SwaggerConfig.java
+++ src/main/java/com/spatel/swagger/swaggerexample/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig {
 
 
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
